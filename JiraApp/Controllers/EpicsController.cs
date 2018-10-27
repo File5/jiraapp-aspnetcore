44ea62d9f598934ef988c4eabc5ebd11372ecfe5
Aleksandr Zuev <zuev08@gmail.com>
2018-10-27 19:10:37 +0300

    Add Status, Projects, Epics, Tasks

---
+++ JiraApp/Controllers/EpicsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JiraApp.Models;
+
+namespace JiraApp.Controllers
+{
+    public class EpicsController : Controller
+    {
+        private readonly JiraContext _context;
+
+        public EpicsController(JiraContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Epics
+        public async Task<IActionResult> Index()
+        {
+            var jiraContext = _context.Epics.Include(e => e.Project);
+            return View(await jiraContext.ToListAsync());
+        }
+
+        // GET: Epics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var epic = await _context.Epics
+                .Include(e => e.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (epic == null)
+            {
+                return NotFound();
+            }
+
+            return View(epic);
+        }
+
+        // GET: Epics/Create
+        public IActionResult Create()
+        {
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id");
+            return View();
+        }
+
+        // POST: Epics/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ProjectId")] Epic epic)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(epic);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", epic.ProjectId);
+            return View(epic);
+        }
+
+        // GET: Epics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var epic = await _context.Epics.FindAsync(id);
+            if (epic == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", epic.ProjectId);
+            return View(epic);
+        }
+
+        // POST: Epics/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ProjectId")] Epic epic)
+        {
+            if (id != epic.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(epic);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EpicExists(epic.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", epic.ProjectId);
+            return View(epic);
+        }
+
+        // GET: Epics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var epic = await _context.Epics
+                .Include(e => e.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (epic == null)
+            {
+                return NotFound();
+            }
+
+            return View(epic);
+        }
+
+        // POST: Epics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var epic = await _context.Epics.FindAsync(id);
+            _context.Epics.Remove(epic);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EpicExists(int id)
+        {
+            return _context.Epics.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ JiraApp/Controllers/TasksController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JiraApp.Models;
+
+namespace JiraApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly JiraContext _context;
+
+        public TasksController(JiraContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tasks
+        public async Task<IActionResult> Index()
+        {
+            var jiraContext = _context.Tasks.Include(t => t.Epic).Include(t => t.Person).Include(t => t.Project).Include(t => t.Status);
+            return View(await jiraContext.ToListAsync());
+        }
+
+        // GET: Tasks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks
+                .Include(t => t.Epic)
+                .Include(t => t.Person)
+                .Include(t => t.Project)
+                .Include(t => t.Status)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // GET: Tasks/Create
+        public IActionResult Create()
+        {
+            ViewData["EpicId"] = new SelectList(_context.Epics, "Id", "Id");
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "Id");
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id");
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id");
+            return View();
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,CreatedAt,ClosedAt,EpicId,ProjectId,StatusId,PersonId")] Models.Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EpicId"] = new SelectList(_context.Epics, "Id", "Id", task.EpicId);
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "Id", task.PersonId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", task.ProjectId);
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id", task.StatusId);
+            return View(task);
+        }
+
+        // GET: Tasks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            ViewData["EpicId"] = new SelectList(_context.Epics, "Id", "Id", task.EpicId);
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "Id", task.PersonId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", task.ProjectId);
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id", task.StatusId);
+            return View(task);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,CreatedAt,ClosedAt,EpicId,ProjectId,StatusId,PersonId")] Models.Task task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(task);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExists(task.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EpicId"] = new SelectList(_context.Epics, "Id", "Id", task.EpicId);
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "Id", task.PersonId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", task.ProjectId);
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id", task.StatusId);
+            return View(task);
+        }
+
+        // GET: Tasks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks
+                .Include(t => t.Epic)
+                .Include(t => t.Person)
+                .Include(t => t.Project)
+                .Include(t => t.Status)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
